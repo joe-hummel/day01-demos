7ca8db1befe27fa473b3d59eb0bfc427ce9f91af
Joe Hummel, PhD <joe.hummel@northwestern.edu>
2025-01-10 13:45:42 -0600

    update Sql demo

--- main-sql.py
+++ main-sql.py
@@ -6,78 +6,52 @@
 # Northwestern University
 #
 
-import datatier
+import pymysql
 from configparser import ConfigParser
 
 
-#################################################################
+print("**Starting**")
+print()
 #
-# execute_select:
+# setup AWS based on config file:
 #
-def execute_select(sql):
-    #
-    print()
-    #
-    # setup AWS based on config file:
-    #
-    config_file = 'movielens-config.ini'    
-    configur = ConfigParser()
-    configur.read(config_file)
-    
-    #
-    # configure for RDS access
-    #
-    rds_endpoint = configur.get('rds', 'endpoint')
-    rds_portnum = int(configur.get('rds', 'port_number'))
-    rds_username = configur.get('rds', 'user_name')
-    rds_pwd = configur.get('rds', 'user_pwd')
-    rds_dbname = configur.get('rds', 'db_name')
+config_file = 'movielens-config.ini'    
+configur = ConfigParser()
+configur.read(config_file)
 
-    #
-    # open connection to the database:
-    #
-    print(">>opening connection to MySQL server in AWS...")
-    
-    dbConn = datatier.get_dbConn(rds_endpoint, rds_portnum, rds_username, rds_pwd, rds_dbname)
-      
-    #
-    # execute the SQL:
-    #
-    print(">>executing query...")
-    rows = datatier.retrieve_all_rows(dbConn, sql)
-    
-    #
-    # close connection and return result:
-    #
-    N = len(rows)
-    
-    if N==1:
-      print(">>retrieved 1 row")
-    else:
-      print(">>retrieved", N, "rows")
-    print()
-    
-    dbConn.close()
-    return rows
-    
+#
+# configure for RDS access
+#
+rds_endpoint = configur.get('rds', 'endpoint')
+rds_portnum = int(configur.get('rds', 'port_number'))
+rds_username = configur.get('rds', 'user_name')
+rds_pwd = configur.get('rds', 'user_pwd')
+rds_dbname = configur.get('rds', 'db_name')
 
-#################################################################
 #
-# main
+# open connection to the database:
 #
-if __name__ == "__main__":
-  #
-  print("**Starting**")
-  print()
-  print("Enter a SELECT query to execute against MovieLens database>")
-  print()
-  
-  sql = input()
-  
-  resultset = execute_select(sql)
-  
-  for row in resultset:
-    print(row)
-  
-  print()
-  print("**Done**")
+print(">>opening connection to MySQL server in AWS...")
+
+#dbConn = pymysql.connect(...)
+                             
+#
+# execute the SQL:
+#
+print(">>executing query...")
+
+sql = """
+      select * from movies limit 10 offset 0;
+      """
+#
+# TODO
+#
+print()
+print("TODO")
+print()
+
+
+
+
+print()
+print("**Done**")
